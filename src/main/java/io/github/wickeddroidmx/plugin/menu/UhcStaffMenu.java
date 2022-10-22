d891fcd2a711a43b550f1a776b3c532c806175c6
Agus5534 <agustinmonte06@gmail.com>
2022-10-22 13:52:39 -0300

    Fixed Times Inventory design error

--- src/main/java/io/github/wickeddroidmx/plugin/menu/UhcStaffMenu.java
+++ src/main/java/io/github/wickeddroidmx/plugin/menu/UhcStaffMenu.java
@@ -24,7 +24,7 @@ public class UhcStaffMenu {
                         "BCCCDCCCB",
                         "BCCHIJCCB",
                         "BCCNOPCCB",
-                        "BCCTUVWCB",
+                        "BCTUVWCCB",
                         "BBBBBBBBB"
                 )
                 .setLayoutItem('B', ItemClickable.builderCancellingEvent()
