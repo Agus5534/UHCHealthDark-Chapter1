993d2d32967f6eeaa86ddcd39c9952e7a80f5041
Agus5534 <agustinmonte06@gmail.com>
2022-11-02 12:21:29 -0300

    TradeParanoia now has own announcement

--- src/main/java/io/github/wickeddroidmx/plugin/modalities/scenarios/TradeParanoia.java
+++ src/main/java/io/github/wickeddroidmx/plugin/modalities/scenarios/TradeParanoia.java
@@ -11,23 +11,25 @@ import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class TradeParanoia extends Modality {
     public TradeParanoia() {
         super(ModalityType.SCENARIO, "trade_paranoia", "&aTrade Paranoia",  Material.EMERALD,
-                ChatUtils.format("&7- Al tradear con un aldeano hay un 5% de que salgan tus coords."));
+                ChatUtils.format("&7- Al tradear con un aldeano hay un 7% de que salgan tus coords."));
     }
 
     @EventHandler
     public void onPlayerTrade(PlayerTradeEvent e) {
         var player = e.getPlayer();
-        var random = new Random();
         var location = player.getLocation();
 
-        if (random.nextInt(100) >= 95) {
+        final String TRADE_PARANOIA = ChatUtils.format("&7[&cTrade Paranoia&7] &7» ");
+
+        if (ThreadLocalRandom.current().nextInt(1,100) <= 7) {
             player.playSound(player.getLocation(), Sound.ENTITY_ARROW_HIT_PLAYER, 1.0F, 1.0F);
 
-            Bukkit.broadcast(Component.text(ChatUtils.PREFIX + ChatUtils.format(String.format("Coordenadas de &6%s &7| X: &6%d &7| Y: &6%d &7| Z: &6%d &7| Mundo: &6%s &7", player.getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), location.getWorld().getName()))));
+            Bukkit.broadcast(Component.text(ChatUtils.format(String.format("&7[&cTrade Paranoia&7] &7» Coordenadas de &6%s &7| X: &6%d &7| Y: &6%d &7| Z: &6%d &7| Mundo: &6%s &7", player.getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), location.getWorld().getName()))));
         }
     }
 }
