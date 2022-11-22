26c6d67f80359d086c1e2bbf9e51453e12c2877d
Agus5534 <agustinmonte06@gmail.com>
2022-11-22 20:45:45 -0300

    Added `GAME_ARENA_COMMAND_EXPERIMENT` experiment

---
+++ src/main/java/io/github/wickeddroidmx/plugin/experiments/list/GAME_ARENA.java
@@ -0,0 +1,10 @@
+package io.github.wickeddroidmx.plugin.experiments.list;
+
+import io.github.wickeddroidmx.plugin.experiments.Experiment;
+
+public class GAME_ARENA extends Experiment {
+
+    public GAME_ARENA() {
+        super("GAME_ARENA_COMMAND_EXPERIMENT", "Activa el comando /arena", false, true);
+    }
+}
