a9c69d8e81a985d9b1127c16f7893412913fbf09
Agus5534 <agustinmonte06@gmail.com>
2023-02-17 12:35:21 -0300

    Fixed UhcIdLoader misleading package

--- src/main/java/io/github/wickeddroidmx/plugin/services/UhcIdLoader.java
+++ src/main/java/io/github/wickeddroidmx/plugin/services/UhcIdLoader.java
@@ -3,7 +3,9 @@ package io.github.wickeddroidmx.plugin.services;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import io.github.wickeddroidmx.plugin.game.GameManager;
 import io.github.wickeddroidmx.plugin.utils.chat.ChatUtils;
+import me.yushust.inject.InjectAll;
 import org.bukkit.Bukkit;
 
 import java.io.InputStream;
@@ -11,27 +13,27 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class UhcIdLoader {
+@InjectAll
+public class UhcIdLoader implements Loader {
 
-    public int getID() {
+    private GameManager gameManager;
+
+    @Override
+    public void load() {
         try {
             int id = getRootObject("https://agus5534.tech/hd-uhcid.json").get("id").getAsInt();
             String date = getRootObject("https://agus5534.tech/hd-uhcid.json").get("last_update").getAsString();
             String s = "Se ha encontrado el UHC ID %s, actualizado por última vez: %s";
 
-            Bukkit.getLogger().info(String.format(s,id,date));
-            Bukkit.broadcast(ChatUtils.formatC(ChatUtils.PREFIX + "&6"+ String.format(s,id,date)));
-
-            return id;
+            gameManager.setUhcId(String.valueOf(id));
         } catch (Exception e) {
             Bukkit.broadcast(ChatUtils.formatC(ChatUtils.PREFIX + "&4Se ha encontrado el UHC ID de emergencia, la petición ha derivado en un error"));
 
             Bukkit.getLogger().severe("Se ha encontrado el UHC ID de emergencia, la petición ha derivado en un error");
 
-            return 253;
+            gameManager.setUhcId("253");
         }
     }
-
 
     private final JsonObject getRootObject(String url) throws Exception{
         URL link = new URL(url);
